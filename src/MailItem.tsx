adde6aaa1a3dbc2a1288c74352698bebbb30177b
Pob <590650@gmail.com>
2019-11-18 01:19:02 +0700

    modify some styles

--- src/MailItem.tsx
+++ src/MailItem.tsx
@@ -20,9 +20,10 @@ interface MailProps {
 // Grid Container
 const MailItemWrapper = styled.article<{ checked: boolean }>`
   border-top: 1px solid #ddd;
-  padding: 10px;
+  padding: 10px 20px;
   display: grid;
   grid-template-columns: 1fr 6fr 1fr;
+  grid-gap: 10px;
   ${props => props.checked && `background-color: #ddd;`}
 `
 
@@ -31,6 +32,9 @@ const Checked = styled.div`
   border: 1px solid blue;
   grid-column: 1 / 2;
   grid-row: 1 / span 3;
+  align-self: center;
+  justify-self: center;
+  cursor: pointer;
 `
 const Sender = styled.div`
   grid-column: 2 / 3;
@@ -38,12 +42,21 @@ const Sender = styled.div`
 const BtnWrapper = styled.div`
   border: 1px solid blue;
   grid-column: 3 / 4;
+  align-self: center;
 `
 const Heading = styled.h2`
   grid-column: 2 / 3;
 `
 const Content = styled.p`
   grid-column: 2 / 3;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`
+
+// Children of Grid Items
+const BtnDelete = styled.button`
+  cursor: pointer;
 `
 
 /**
@@ -55,7 +68,7 @@ const MailItem: React.FC<MailProps> = props => {
       <Checked onClick={props.onClick}>{`${props.checked}`}</Checked>
       <Sender>{props.sender}</Sender>
       <BtnWrapper>
-        <button onClick={props.handleDelete}>DEL</button>
+        <BtnDelete onClick={props.handleDelete}>DEL</BtnDelete>
       </BtnWrapper>
       <Heading>{props.subject}</Heading>
       <Content>{props.body}</Content>
